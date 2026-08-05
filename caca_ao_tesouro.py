25d4132b685176d0afa35c85c58c192df03d2f5f
Kaleb567 <kalebdantas179@gmail.com>
2026-08-05 19:22:18 -0300

    loop do jogo

--- caca_ao_tesouro.py
+++ caca_ao_tesouro.py
@@ -1,22 +1,41 @@
 # Jogo caça ao tesouro usando Pygame
 
 import pygame
-import random
+
+from pygame.locals import *
+# ('*' Serve para dizer que está importando constantes e os sub modulos)
+
+from sys import exit
+# Serve para fechar a janela 
+
+pygame.init()
+# Serve para inicializar todas as funções e variaveis
+
+pygame.display.set_caption('JOGO')
+# Serve para mudar o nome do jogo
+
 import cores
 
 
 def main():
-   pygame.init()
 
    fonte = pygame.font.SysFont("Comic Sams MS", 30)
 
-   tela = pygame.display.set_mode(1000, 800)
+   tela = pygame.display.set_mode((1000, 800))
    pygame.display.set_caption( "Campo minado" )
 
-   tela.fill(cores.verde)
+   tela.fill(cores.marrom)
 
-   pygame.display.update()
-
+   while True:
+   # Loop do jogo pois o jogo tem que atualizar  
+      for event in pygame.event.get():
+      # LOOP for ser ve para verificar se algum evento ocorreu 
+         if event.type == QUIT:
+            pygame.quit()
+            exit()
+            # comando para fechar o jogo
+      pygame.display.update()
+    # serve para atualizar a tela do jogo caso aja uma interação
 
 
 
